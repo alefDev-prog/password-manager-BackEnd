548c26e227a7cfbcac41e75fb806e54b1362f443
alefCS50 <allan.efendic@icloud.com>
2023-04-14 17:44:47 +0200

    feat: contact with front-end

--- build/server.js
+++ build/server.js
@@ -4,7 +4,24 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
+const cors_1 = __importDefault(require("cors"));
 const app = (0, express_1.default)();
+//Middlewares
+app.use(express_1.default.json());
+app.use((0, cors_1.default)());
+//requests
+app.get('/', (req, res) => {
+    res.send("default message");
+});
+app.post('/register', (req, res) => {
+    const { username, password } = req.body;
+    console.log(req.body);
+    res.json({
+        username,
+        password
+    });
+});
+//port
 app.listen(5000, () => {
     console.log("Server listening on port 5000");
 });
--- server.ts
+++ server.ts
@@ -1,11 +1,31 @@
 import express from "express";
-
+import cors from 'cors';
 const app = express();
 
-app.use(()=> {
-    console.log("hej");
+//Middlewares
+app.use(express.json());
+app.use(cors());
+
+//requests
+app.get('/', (req, res)=> {
+    res.send("default message");
+});
+
+app.post('/register', (req, res) => {
+    const {username, password} = req.body;
+    console.log(req.body);
+    
+    res.json({
+        username,
+        password
+    });
 })
+
+
+
+
+//port
 app.listen(5000, ():void => {
     console.log("Server listening on port 5000");
-})
+});
 
