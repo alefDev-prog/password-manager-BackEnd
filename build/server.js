557462a9f8bcf497938480ab2a370540742382cd
alefCS50 <allan.efendic@icloud.com>
2023-04-17 15:19:24 +0200

    feat: added basic sessions

--- build/server.js
+++ build/server.js
@@ -1,13 +1,4 @@
 "use strict";
-var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
-    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
-    return new (P || (P = Promise))(function (resolve, reject) {
-        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
-        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
-        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
-        step((generator = generator.apply(thisArg, _arguments || [])).next());
-    });
-};
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
@@ -17,39 +8,69 @@ const cors_1 = __importDefault(require("cors"));
 const mongoose_1 = __importDefault(require("mongoose"));
 const database_1 = require("./database");
 const bcryptjs_1 = __importDefault(require("bcryptjs"));
+const express_session_1 = __importDefault(require("express-session"));
 const dotenv = require('dotenv').config();
 const app = (0, express_1.default)();
+//env variables
+const { DB_URI, SESSION_SECRET, PORT, SESSION_NAME } = process.env;
 //connect to db
-mongoose_1.default.connect(`${process.env.DB_URL}`);
+mongoose_1.default.connect(`${DB_URI}`);
 //Middlewares
 app.use(express_1.default.json());
 app.use((0, cors_1.default)());
+app.use((0, express_session_1.default)({
+    name: `${SESSION_NAME}`,
+    secret: `${SESSION_SECRET}`,
+    saveUninitialized: false,
+    resave: false,
+    cookie: {
+        maxAge: 1,
+        sameSite: true
+    }
+}));
+function isAuthenticated(req, res, next) {
+    if (!req.session.user) {
+        res.json("Not authenticated");
+    }
+}
 //requests
 app.get('/', (req, res) => {
     res.send("hello");
 });
-app.post('/register', (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+app.get('/data', isAuthenticated, (req, res) => {
+    res.json("You are authenticated");
+});
+app.post('/register', async (req, res) => {
     const { password, email } = req.body;
-    const user = yield database_1.UserModel.create({
+    const user = await database_1.UserModel.create({
         email,
         password: bcryptjs_1.default.hashSync(password)
     });
     res.json({
         user
     });
-}));
-app.post('/login', (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+});
+app.post('/login', async (req, res) => {
     const { email, password } = req.body;
-    const user = yield database_1.UserModel.findOne({ email });
+    const user = await database_1.UserModel.findOne({ email });
     if (user) {
         const CorrectPass = bcryptjs_1.default.compareSync(password, user.password);
-        CorrectPass ? res.json('Password OK') : res.json('Password incorrect');
+        if (CorrectPass) {
+            //store session
+            req.session.user = user._id;
+            res
+                .status(202)
+                .json('Password OK');
+        }
+        else {
+            res.status(406).json('Password incorrect');
+        }
     }
     else {
-        res.json('No matches');
+        res.status(400).json('No matches');
     }
-}));
+});
 //port
-app.listen(process.env.PORT, () => {
+app.listen(PORT, () => {
     console.log("Server listening on port 5000");
 });
--- server.ts
+++ server.ts
@@ -3,21 +3,63 @@ import cors from 'cors';
 import mongoose from 'mongoose';
 import { UserModel } from "./database";
 import bcrypt from 'bcryptjs';
+import session from 'express-session';
+
 
 
 const dotenv = require('dotenv').config();
 const app = express();
 
 
+//TS
+declare module "express-session" {
+    interface SessionData {
+      user: any;
+    }
+}
+
+
+
+
+//env variables
+const {
+    DB_URI,
+    SESSION_SECRET,
+    PORT,
+    SESSION_NAME
+} = process.env;
+
+
 
 //connect to db
-mongoose.connect(`${process.env.DB_URL}`);
+mongoose.connect(`${DB_URI}`);
 
 
 
 //Middlewares
 app.use(express.json());
 app.use(cors());
+
+app.use(session({
+    name: `${SESSION_NAME}`,
+    secret: `${SESSION_SECRET}`,
+    saveUninitialized: false,
+    resave: false,
+    cookie: {
+        maxAge: 1,
+        sameSite: true
+    }
+    
+
+}))
+
+function isAuthenticated(req: any, res: any, next: any) {
+    if(!req.session.user) {
+        res.json("Not authenticated")
+    }
+}
+
+
 
 //requests
 app.get('/', (req, res)=> {
@@ -26,6 +68,9 @@ app.get('/', (req, res)=> {
 });
 
 
+app.get('/data', isAuthenticated, (req, res) => {
+    res.json("You are authenticated");
+})
 
 app.post('/register', async (req, res) => {
     const {password, email} = req.body;
@@ -44,12 +89,23 @@ app.post('/register', async (req, res) => {
 app.post('/login', async (req, res) => {
     const {email, password} = req.body;
     const user = await UserModel.findOne({email});
+
     if(user) {
         const CorrectPass: boolean = bcrypt.compareSync(password, user.password);
-        CorrectPass ? 
-        res.status(202).json('Password OK') 
-        :
-        res.status(406).json('Password incorrect');
+        if(CorrectPass){ 
+            
+
+            //store session
+            req.session.user = user._id;
+
+            res
+            .status(202)
+            .json('Password OK') 
+        }
+        else {
+            res.status(406).json('Password incorrect');
+        }
+        
 
     }
     else {
@@ -61,7 +117,7 @@ app.post('/login', async (req, res) => {
 
 
 //port
-app.listen(process.env.PORT, ():void => {
+app.listen(PORT, ():void => {
     console.log("Server listening on port 5000");
 });
 
