58a7be2fd67e918fe02aa0524b5a953473f6781b
alefCS50 <allan.efendic@icloud.com>
2023-04-28 09:51:38 +0200

    feat: console check

--- src/middleware/authenticate.ts
+++ src/middleware/authenticate.ts
@@ -1,6 +1,7 @@
 import { Request, Response, NextFunction } from 'express';
 
 export default function isAuthenticated(req:Request, res:Response, next:NextFunction) {
+    console.log(req.session);
     if(!req.session.user) {
         res.json("Not authenticated");
     }
